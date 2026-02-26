934eb207ba302039bc856b84a97009e624dc0a0b
OishiKunare <nadeeffy@gmail.com>
2026-02-27 06:53:33 +0700

    Prevent Redundant Encrypt Password

--- models/User.js
+++ models/User.js
@@ -45,7 +45,8 @@ const UserSchema = new mongoose.Schema({
 
 // Encrypt password only when it is modified
 UserSchema.pre("save", async function (next) {
-  
+  if (!this.isModified('password'))
+    return next();
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   
