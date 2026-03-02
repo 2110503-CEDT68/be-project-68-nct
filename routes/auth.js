a2565e97bd31108d699677fb0e7f176586495a4d
OishiKunare <nadeeffy@gmail.com>
2026-03-02 15:58:28 +0700

    Edit router auth

--- routes/auth.js
+++ routes/auth.js
@@ -16,7 +16,7 @@ router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
 router.get('/logout', logout);
-router.put('/changepassword', protect, changePassword);
+router.put('/changePassword', protect, changePassword);
 router.delete('/delete/:id', protect, authorize('admin'), deleteUser);
 
 module.exports = router;
